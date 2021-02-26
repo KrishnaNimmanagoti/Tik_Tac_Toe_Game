92f2f3e2f8fcb019e7103e7ebd85b85034ca996f
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-02-26 10:47:31 +0530

    refactored to take user symbol

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
+
 public class TicTacToeGame {
+
+	public static char[] board;
+	public static String computerTurn;
+	public static String playerTurn;
 
 	public static char[] createBoard() {
 
@@ -10,27 +16,37 @@ public class TicTacToeGame {
 
 		}
 
-		System.out.println("|---|---|---|");
-
-		System.out.println("|   |   |   |");
-
-		System.out.println("|-----------|");
-
-		System.out.println("|   |   |   |");
-
-		System.out.println("|-----------|");
-
-		System.out.println("|   |   |   |");
-
-		System.out.println("|---|---|---|");
-
 		return board;
 
+	}
+
+	public static void Turn() {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a x or y");
+
+		playerTurn = sc.next();
+
+
+	if (playerTurn.equals("x")) {
+
+			computerTurn = "O";
+
+		}
+
+		else {
+
+			computerTurn = "X";
+
+		}
 	}
 
 	public static void main(String[] args) {
 
 		char[] board = createBoard();
+
+		Turn();
 
 	}
 
